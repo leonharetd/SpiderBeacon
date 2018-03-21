8260d2a2830fa429f5c3a68109e645b454c57b1b
Administrator <610288372@qq.com>
2018-03-21 23:11:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env Python
+# coding:utf-8
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
+
+define("port", default=8000, help="run on the given port", type=int)
+
+routes = [
+    (r"/", ""),  # 来自根路径的请求用 IndesHandlers 处理
+    (r"/qiwsir/(.*)", ""),
+]
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', welcome you to read: www.itdiffer.com')
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()

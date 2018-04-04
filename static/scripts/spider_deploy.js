9b08ce8edc20f4da8efbdc412ac3d1a5b2f4d218
IzanaB <IzanaB@outlook.com>
2018-04-05 02:53:09 +0800

    1`样式与js。

--- static/scripts/spider_deploy.js
+++ static/scripts/spider_deploy.js
@@ -1,3 +1,9 @@
+function get_deploy_progress(){
+    $.ajax({
+        url:"",
+
+    })
+}
 
 //获取project_info的值
 function get_ProInfo_value(){
@@ -48,16 +54,21 @@ $(".btn-info").click(function(){
     var dingValue = null;
 
     if(!projectValue){
+//        form_error();
+//        select1_error();
         alert("项目名不可以为空");
     }
 
     if(!spiderValue){
+//        form_error();
+//        select2_error();
         alert("爬虫项目名不可以为空");
     }
 
     if ($('#optionsRadios1').is(':checked')) {
         dingValue = $("#dingValue").val();
         if(!dingValue){
+//            form_error();
             alert("定时任务不可以为空。");
         }
     }
@@ -68,8 +79,7 @@ $(".btn-info").click(function(){
         server_value.push($(this).text());
     });
     if(!Array.isArray(server_value) || server_value.length == 0){
-//        $('.form-group').addClass("has-error");
-//        $('.help-block').show();
+//        form_error();
         alert("请选择要部署的服务器");
     }
 
@@ -78,6 +88,9 @@ $(".btn-info").click(function(){
     datas['spider'] = spiderValue;
     datas['dingValue'] = dingValue;
     datas['servers'] = server_value;
+
+    $(".deploy_progress").show();
+
     $.ajax({
         url : '',
         data : datas,
@@ -85,9 +98,33 @@ $(".btn-info").click(function(){
         dataType : 'json',
         async: true,
         success : function(data) {
-            alert(data.result);
-            $('.modal').modal('hide')
-            location.reload();
+            if(data.status == 'ok'){
+
+            }
+
         }
     });
 })
+
+
+//function select1_error(){
+//    $("#projectValue").css("border:1px solid #ff0000;");
+//}
+//function select1_success(){
+//    $("#projectValue").css("border:1px solid #aaa;");
+//}
+//
+//function select2_error(){
+//    $("#spiderValue").css("border:1px solid #ff0000;");
+//}
+//function select2_success(){
+//    $("#spiderValue").css("border:1px solid #aaa;");
+//}
+//
+//function form_error(){
+//    $('.form-group').addClass("has-error");
+//}
+//
+//function form_success(){
+//    $('.form-group').removeClass("has-error");
+//}
